07cacefbf9323559f8d58dc2b390e2be268e5dd3
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-07-06 11:02:00 -0700

    added id data to Users.js, deleted accidental typo on www

--- bin/www.js
+++ bin/www.js
@@ -12,6 +12,7 @@ app.set("port", process.env.PORT || 3000);
 //can use in sync({if not exists}) = tables created
 db.sequelize.sync().then(function() {
   // set our app to listen to the port we set above
+
   var server = app.listen(app.get("port"), function() {
     // then save a log of the listening to our debugger.
     debug("Express server listening on port " + server.address().port);
