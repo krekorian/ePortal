7894b8004fa0159504e385ed14f471febdc4dc28
Rachel Rodriguez <rachelrodriguez014@gmail.com>
2019-07-08 21:02:55 -0700

    updates to request routes and requests controller

---
+++ controllers/requests_controller.js
@@ -0,0 +1,5 @@
+exports.request = function (req, res) {
+
+    console.log("requests controller");
+    res.render('requests/requests');
+};
--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 module.exports = function (app) {
   const login = require("./routes/login");
   const dashboard = require("./routes/dashboard");
+  const requests = require('./routes/requests')
   console.log("REaching main routes file");
   app.use("/", login);
+  app.use('/requests', requests)
   // app.use("/dashboard", dashboard);
 };
